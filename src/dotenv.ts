66c3545e03c21ffe4bc6d33d560c7674e6485f3f
Hadriel Benjo <hdriel@gmail.com>
2025-06-27 12:20:13 +0300

    update files

--- src/dotenv.ts
+++ src/dotenv.ts
@@ -3,6 +3,7 @@ import { expand } from 'dotenv-expand';
 import { ENV_FILE_PATH } from './paths';
 
 console.debug('ENV_FILE_PATH', ENV_FILE_PATH);
+
 const myEnv = dotenv.config({ path: ENV_FILE_PATH });
 expand(myEnv);
 
